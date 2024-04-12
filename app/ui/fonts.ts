1ba8807379b1b8652f39303c16d2d84f8c3eacb9
Oury Diallo <amadoury313@gmail.com>
2024-04-12 15:58:09 +0000

    initial layout added

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Inter, Lusitana} from "next/font/google"
+
+export const interFont = Inter({subsets: ["latin"]})
+export const lusitana = Lusitana({weight: ["400", "700"], subsets: ["latin"]}) 
